130d47e2e4b76f8ba932eaca79c711625c1d93be
Jhoner <jhonerquispe31@gmail.com>
2026-08-14 08:47:39 -0500

    Proyecto inicial

---
+++ repaso1array/src/main/java/pe/edu/upeu/repaso1array/Launcher.java
@@ -0,0 +1,9 @@
+package pe.edu.upeu.repaso1array;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
