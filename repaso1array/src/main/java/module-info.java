8f04732ce6cb2d55da275425bfd9de29a064acf4
luz-montalvo <luz.montalvo@upeu.edu.pe>
2026-08-31 14:23:57 -0500

    Concetos de POO

--- repaso1array/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pe.edu.upeu.repaso1array {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens pe.edu.upeu.repaso1array to javafx.fxml;
-    exports pe.edu.upeu.repaso1array;
-}
