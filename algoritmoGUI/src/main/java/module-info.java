e6cc5a3cc0ab20b752c0c229bd9a65784fa12335
luz-montalvo <luz.montalvo@upeu.edu.pe>
2026-09-04 09:11:00 -0500

    proyecto limpiado

--- algoritmoGUI/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pe.edu.upeu.algoritmogui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens pe.edu.upeu.algoritmogui to javafx.fxml;
-    exports pe.edu.upeu.algoritmogui;
-}
