73c6032a93fe720269b94bbb8b157d9687d48568
luz-montalvo <luz.montalvo@upeu.edu.pe>
2026-09-07 09:46:58 -0500

    Modelo completado

--- Sysventas/src/main/java/module-info.java
+++ Sysventas/src/main/java/module-info.java
@@ -5,6 +5,9 @@ module pe.edu.upeu.sysventas {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
 
+    requires static lombok;
+    requires jakarta.validation;
+
     opens pe.edu.upeu.sysventas to javafx.fxml;
     exports pe.edu.upeu.sysventas;
 }
