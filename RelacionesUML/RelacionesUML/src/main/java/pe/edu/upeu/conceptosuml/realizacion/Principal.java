77295f017dc9dfaf8c0f960f30dac332841e63e6
luz-montalvo <luz.montalvo@upeu.edu.pe>
2026-09-14 02:36:54 -0500

    Conceptos de RelacionesUML

---
+++ RelacionesUML/RelacionesUML/src/main/java/pe/edu/upeu/conceptosuml/realizacion/Principal.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.conceptosuml.realizacion;
+
+public class Principal {
+    public static main(String[]args){
+        Volador volador= new Avion();
+        volador.correr();
+        volador.new volar();
+        volador.correr();
+        volador.correr();
+
+    }
+
+}
